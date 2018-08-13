b8be7ef46390715d41e2e7604fa1587e1eef05a9
Leo Mauro <lmauro@usb.ve>
2018-08-13 14:11:18 -0400

    Annotate and document functions.

--- entropy.py
+++ entropy.py
@@ -5,20 +5,37 @@ import numpy as np
 from PIL import Image
 
 
-def entropy(band):
+def entropy(band: np.ndarray) -> np.float64:
+    """
+    Compute the entropy content of an image's band.
+
+    :param band: The band data to analyze.
+    :return:     Data entropy in bits.
+    """
     hist, _ = np.histogram(band, bins=range(0, 256))
     hist = hist[hist > 0]
     return -np.log2(hist / hist.sum()).sum()
 
 
-def show_entropy(band_name, band):
+def show_entropy(band_name: str, band: np.ndarray) -> None:
+    """
+    Analyze and display entropy of an image's band.
+
+    :param band_name: The name of the band to analyze.
+    :param band:      The band data to analyze.
+    """
     bits = entropy(band)
     per_pixel = bits / band.size
     print("{:3s} entropy = {:.2f} bits, {:.6f} per pixel"
           .format(band_name, bits, per_pixel))
 
 
-def process(img_file):
+def process(img_file: str) -> None:
+    """
+    Process the image file.
+
+    :param img_file: The image file.
+    """
     im = Image.open(img_file)
     print(img_file, im.format, im.size, im.mode)
     print()
